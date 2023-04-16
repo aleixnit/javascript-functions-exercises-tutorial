bbbe5a41cba601420bf0dd8654382f4b46fa2379
Aleix Aguilar <nitjac210@gmail.com>
2023-04-16 10:57:03 +0000

    Ejercicios 9/10 100% completados

--- exercises/09-Array-Methods/app.js
+++ exercises/09-Array-Methods/app.js
@@ -1,6 +1,7 @@
 var names = ['John', 'Kenny', 'Tom', "Bob", 'Dilan'];
 
 function sortNames(arr){
-
+    arr.sort();
+    return arr;
 }
 console.log(sortNames(names));
--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,6 +1,19 @@
 const rapid = (myString) => {
     // Your code goes here
-};
+    let newString = '';
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    
+    for (let i = 0; i < myString.length; i++) {
+      let currentChar = myString[i].toLowerCase();
+      
+      // Comprobar si la letra actual es una vocal
+      if (!vowels.includes(currentChar)) {
+        newString += currentChar.toUpperCase();
+      }
+    }
+    
+    return newString;
+  };
 
 // Work above this line; do not change code below
 let str = "John";
